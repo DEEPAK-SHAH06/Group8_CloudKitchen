215698477e8a1720c1ecb9bbd09c02df1afc7c34
DEEPAK-SHAH06 <shah.deepak6969@gmail.com>
2025-12-02 07:33:20 +0545

    Database Code is added by Scrum Master

---
+++ src/database/Databasee.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author deepakshah
+ */
+public interface Databasee {
+    Connection openConnection();
+    void closeConnection(Connection conn);
+    ResultSet runQuery(Connection conn, String query);
+    int executeUpdate(Connection conn, String query);
+    
+}

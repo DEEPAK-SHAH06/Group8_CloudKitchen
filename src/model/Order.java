89fdd81f0d9e894d11725c2c054f72cbd5843283
unknown <ghimirepurnima570@gmail.com>
2025-12-15 10:05:24 +0545

    doing commit for Sprint 3 - Monday

--- src/model/Order.java
+++ src/model/Order.java
@@ -8,6 +8,68 @@ package model;
  *
  * @author deepakshah
  */
+import java.time.LocalDateTime;
+
 public class Order {
     
+    private int order_id;
+    private Customers customer_id;
+    private double totalAmount;
+    private LocalDateTime orderTime;
+
+    private OrderStatus orderStatus;
+    private enum OrderStatus{
+        PENDING,
+        CONFIRMED,
+        PREPARING,
+        OUT_FOR_DELIVERY,
+        DELIVERED,
+        CANCELLED
+    }
+    public int getOrder_id() {
+        return order_id;
+    }
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+    public Customers getCustomer_id() {
+        return customer_id;
+    }
+    public void setCustomer_id(Customers customer_id) {
+        this.customer_id = customer_id;
+    }
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+    public Order(int order_id, Customers customer_id, double totalAmount, LocalDateTime orderTime,
+            Order.OrderStatus orderStatus) {
+        this.order_id = order_id;
+        this.customer_id = customer_id;
+        this.totalAmount = totalAmount;
+        this.orderTime = orderTime;
+        this.orderStatus = orderStatus;
+    }
+
+    
+
+
+
+
+
 }
+
--- src/model/Reports.java
+++ src/model/Reports.java
@@ -4,10 +4,71 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author deepakshah
  */
 public class Reports {
     
+    private int report_id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String totalOrder;
+    private String totalRevenue;
+    private String mostSoldItem;
+
+    public int getReport_id() {
+        return report_id;
+    }
+    public void setReport_id(int report_id) {
+        this.report_id = report_id;
+    }
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+    public String getTotalOrder() {
+        return totalOrder;
+    }
+    public void setTotalOrder(String totalOrder) {
+        this.totalOrder = totalOrder;
+    }
+    public String getTotalRevenue() {
+        return totalRevenue;
+    }
+    public void setTotalRevenue(String totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+    public String getMostSoldItem() {
+        return mostSoldItem;
+    }
+    public void setMostSoldItem(String mostSoldItem) {
+        this.mostSoldItem = mostSoldItem;
+    }
+    public Reports(LocalDate startDate, LocalDate endDate, String totalOrder, String totalRevenue,
+            String mostSoldItem) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.totalOrder = totalOrder;
+        this.totalRevenue = totalRevenue;
+        this.mostSoldItem = mostSoldItem;
+    }
+
+    
+    
+
+
+
+
 }
+

fc1e005c03f844380332ed9344d14850d89e6fd1
DEEPAK-SHAH06 <shah.deepak6969@gmail.com>
2025-12-19 19:40:24 +0545

    Some changes made in cloudkitchen.java main method

--- src/group8_cloudkitchen/Group8_CloudKitchen.java
+++ src/group8_cloudkitchen/Group8_CloudKitchen.java
@@ -10,12 +10,22 @@ package group8_cloudkitchen;
  */
 import view.login;
 import controller.LoginController;
+import database.Databasee;
+import database.MySqlConnection;
 public class Group8_CloudKitchen {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        
+        // Databasee db = new MySqlConnection();
+        // if(db.openConnection() != null){
+        //     System.out.println("Connection Opened");
+        // }else{
+        //     System.out.println("Connection Closed");
+        // }
         
         // TODO code application logic here
         login loginView = new login();
